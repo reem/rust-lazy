29ec9be87a811632e20a47aa287e4305d356cb91
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-29 15:59:24 -0700

    (init)

---
+++ src/lib.rs
@@ -0,0 +1,7 @@
+ #![license = "MIT"]
+ #![deny(missing_doc)]
+ #![deny(unnecessary_qualification, non_camel_case_types,
+         unused_variable, unnecessary_typecast)]
+
+//! Crate comment goes here
+
