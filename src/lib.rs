ccc112fa2d3631bfc65a273fe5030173ec41aae8
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-29 20:05:22 -0700

    (fix) Refactored to avoid dropping uninit memory.

--- src/lib.rs
+++ src/lib.rs
@@ -2,16 +2,44 @@
  #![deny(missing_doc)]
  #![deny(warnings)]
 
-#![feature(unsafe_destructor)]
+#![feature(unsafe_destructor, macro_rules)]
 
 //! Lazy evaluation for Rust.
 
-use std::mem::{transmute, uninitialized};
-use std::ptr::replace;
+use std::mem::{transmute, uninitialized, forget};
+use std::ptr::{replace, copy_nonoverlapping_memory};
 
 /// A lazily evaluated value.
 pub struct Thunk<T> {
     inner: *mut Inner<T>,
+}
+
+#[macro_exports]
+macro_rules! lazy (
+    ($e:expr) => {
+        Thunk::new(proc() { $e })
+    }
+)
+
+impl<T> Thunk<T> {
+    /// Create a lazily evaluated value from a proc that returns that value.
+    pub fn new(producer: proc() -> T) -> Thunk<T> {
+        Thunk { inner: unsafe { transmute(box Unevaluated(producer)) } }
+    }
+
+    /// Force evaluation of a thunk.
+    pub fn force(&self) {
+        let mut inner = unsafe { uninitialized() };
+        unsafe { copy_nonoverlapping_memory(&mut inner, self.inner as *const Inner<T>, 1) };
+        match inner {
+            Evaluated(val) => unsafe {
+                forget(replace(self.inner, Evaluated(val)));
+            },
+            Unevaluated(producer) => unsafe {
+                forget(replace(self.inner, Evaluated(producer())));
+            }
+        }
+    }
 }
 
 enum Inner<T> {
@@ -35,39 +63,16 @@ impl<T> Inner<T> {
     }
 }
 
-impl<T> Thunk<T> {
-    /// Create a lazily evaluated value from a proc that returns that value.
-    pub fn new(producer: proc() -> T) -> Thunk<T> {
-        Thunk { inner: unsafe { transmute(box Unevaluated(producer)) } }
-    }
-}
-
 impl<T> Deref<T> for Thunk<T> {
     fn deref<'a>(&'a self) -> &'a T {
-        let inner = unsafe { replace(self.inner, uninitialized()) };
-        match inner {
-            Evaluated(val) => unsafe {
-                *self.inner = Evaluated(val);
-            },
-            Unevaluated(producer) => unsafe {
-                *self.inner = Evaluated(producer());
-            }
-        }
+        self.force();
         unsafe { (*self.inner).unwrap() }
     }
 }
 
 impl<T> DerefMut<T> for Thunk<T> {
     fn deref_mut<'a>(&'a mut self) -> &'a mut T {
-        let inner = unsafe { replace(self.inner, uninitialized()) };
-        match inner {
-            Evaluated(val) => unsafe {
-                *self.inner = Evaluated(val);
-            },
-            Unevaluated(producer) => unsafe {
-                *self.inner = Evaluated(producer());
-            }
-        }
+        self.force();
         unsafe { (*self.inner).unwrap_mut() }
     }
 }
