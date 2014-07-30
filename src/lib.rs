c3434c18561880c9e95adf1cc380bbb5faf68660
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-29 19:24:24 -0700

    (feat) Added Thunk constructor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,30 @@
  #![license = "MIT"]
  #![deny(missing_doc)]
- #![deny(unnecessary_qualification, non_camel_case_types,
-         unused_variable, unnecessary_typecast)]
+ #![deny(warnings)]
 
 //! Crate comment goes here
+
+//! Lazy evaluation for Rust.
+
+use std::mem::{transmute, uninitialized};
+use std::ptr::replace;
+
+/// A lazily evaluated value.
+pub struct Thunk<T> {
+    inner: *mut Inner<T>,
+}
+
+enum Inner<T> {
+    Evaluated(T),
+    Unevaluated(proc() -> T)
+}
+
+
+impl<T> Thunk<T> {
+    /// Create a lazily evaluated value from a proc that returns that value.
+    pub fn new(producer: proc() -> T) -> Thunk<T> {
+        Thunk { inner: unsafe { transmute(box Unevaluated(producer)) } }
+    }
+}
+
 
