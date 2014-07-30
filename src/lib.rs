ff80127d6047a6da77a8fa0808087f2675a217f0
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-30 00:46:47 -0700

    (refactor) Refactored to use UnsafeCell and avoid heap allocations.

--- src/lib.rs
+++ src/lib.rs
@@ -6,15 +6,16 @@
 
 //! Lazy evaluation for Rust.
 
-use std::mem::{transmute, uninitialized, forget};
+use std::mem::{uninitialized, forget};
 use std::ptr::{replace, copy_nonoverlapping_memory};
+use std::cell::UnsafeCell;
 
 /// A sometimes-cleaner name for a lazily evaluated value.
 pub type Lazy<T> = Thunk<T>;
 
 /// A lazily evaluated value.
 pub struct Thunk<T> {
-    inner: *mut Inner<T>,
+    inner: UnsafeCell<Inner<T>>,
 }
 
 #[macro_exports]
@@ -27,18 +28,18 @@ macro_rules! lazy (
 impl<T> Thunk<T> {
     /// Create a lazily evaluated value from a proc that returns that value.
     pub fn new(producer: proc() -> T) -> Thunk<T> {
-        Thunk { inner: unsafe { transmute(box Unevaluated(producer)) } }
+        Thunk { inner: UnsafeCell::new(Unevaluated(producer)) }
     }
 
     /// Force evaluation of a thunk.
     pub fn force(&self) {
         unsafe {
             let mut inner = uninitialized();
-            copy_nonoverlapping_memory(&mut inner, self.inner as *const Inner<T>, 1);
+            copy_nonoverlapping_memory(&mut inner, self.inner.get() as *const Inner<T>, 1);
             match inner {
                 Evaluated(val) => { forget(val) },
                 Unevaluated(producer) => {
-                    forget(replace(self.inner, Evaluated(producer())));
+                    forget(replace(self.inner.get(), Evaluated(producer())));
                 }
             }
         }
@@ -48,8 +49,8 @@ impl<T> Thunk<T> {
     pub fn unwrap(self) -> T {
         self.force();
         unsafe {
-            match *transmute::<*mut Inner<T>, Box<Inner<T>>>(self.inner) {
-                Evaluated(val) => { forget(self); val },
+            match self.inner.unwrap() {
+                Evaluated(val) => { val },
                 _ => unreachable!()
             }
         }
@@ -64,7 +65,7 @@ enum Inner<T> {
 impl<T> Deref<T> for Thunk<T> {
     fn deref<'a>(&'a self) -> &'a T {
         self.force();
-        match unsafe { &*self.inner } {
+        match unsafe { &*self.inner.get() } {
             &Evaluated(ref val) => val,
             _ => unreachable!()
         }
@@ -74,18 +75,10 @@ impl<T> Deref<T> for Thunk<T> {
 impl<T> DerefMut<T> for Thunk<T> {
     fn deref_mut<'a>(&'a mut self) -> &'a mut T {
         self.force();
-        match unsafe { &mut *self.inner } {
+        match unsafe { &mut *self.inner.get() } {
             &Evaluated(ref mut val) => val,
             _ => unreachable!()
         }
-    }
-}
-
-#[unsafe_destructor]
-impl<T> Drop for Thunk<T> {
-    fn drop(&mut self) {
-        let inner: Box<Inner<T>> = unsafe { transmute(self.inner) };
-        drop(inner);
     }
 }
 
