dd6dbb73138fc0d17eaa37485a5ba5f5fe55f8eb
Jonathan Reem <jonathan.reem@gmail.com>
2014-07-31 14:46:12 -0700

    (refactor) Don't use UnsafeCell for SharedThunk.

--- src/shared.rs
+++ src/shared.rs
@@ -1,49 +1,43 @@
-use std::cell::UnsafeCell;
 use std::sync::RWLock;
-use std::ptr;
+use std::mem;
 
 /// A sometimes cleaner name.
 pub type SharedLazy<T> = SharedThunk<T>;
 
 /// Shareable, sendable lazy data.
 pub struct SharedThunk<T> {
-    inner: RWLock<UnsafeCell<SharedInner<T>>>
+    inner: RWLock<SharedInner<T>>
 }
 
-impl<T: Send> SharedThunk<T> {
+impl<T: Send + Share> SharedThunk<T> {
     /// Create a new shared thunk.
-    pub fn new(producer: proc(): Send -> T) -> SharedThunk<T> {
-        SharedThunk { inner: RWLock::new(UnsafeCell::new(Unevaluated(producer))) }
+    pub fn new(producer: proc(): Send + Share -> T) -> SharedThunk<T> {
+        SharedThunk { inner: RWLock::new(Unevaluated(producer)) }
     }
 
     /// Force evaluation of a thunk.
     pub fn force(&self) {
-        unsafe {
-            // Take out only a read lock.
-            match *self.inner.read().get() {
-                // Already forced. We're done and only took a read lock.
-                Evaluated(_) => return,
-                EvaluationInProgress => {
-                    // Block until evaluation is over.
-                    let _ = self.inner.write();
+        // Take out only a read lock.
+        match *self.inner.read() {
+            // Already forced. We're done and only took a read lock.
+            Evaluated(_) => return,
+            EvaluationInProgress => unreachable!(),
+            // First ones here. Evaluate.
+            Unevaluated(_) => ()
+        }
 
-                    // Don't evaluate again.
-                    return
-                },
+        // Get a write lock for the entire evaluation period.
+        let mut write_lock = self.inner.write();
 
-                // We have to evaluate the producer.
-                Unevaluated(_) => ()
-            }
+        // Set the status to EvaluationInProgress
+        match mem::replace(&mut *write_lock, EvaluationInProgress) {
+            // Get the producer, evaluate it.
+            Unevaluated(producer) => *write_lock = Evaluated(producer()),
+            _ => unreachable!()
+        };
+    }
+}
 
-            // Get a write lock for the entire evaluation period.
-            let mut write_lock = self.inner.write();
-
-            // Set the status to EvaluationInProgress
-            match ptr::replace(write_lock.get(), EvaluationInProgress) {
-                // Get the producer, evaluate it.
-                Unevaluated(producer) => *write_lock.get() = Evaluated(producer()),
-                _ => unreachable!()
-            };
         }
     }
 }
@@ -51,6 +45,6 @@ impl<T: Send> SharedThunk<T> {
 enum SharedInner<T> {
     Evaluated(T),
     EvaluationInProgress,
-    Unevaluated(proc(): Send -> T)
+    Unevaluated(proc(): Send + Share -> T)
 }
 
