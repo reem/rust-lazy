4cbbe18f09f0fe687c7915c267ff203a7b93185e
Jonathan Reem <jonathan.reem@gmail.com>
2014-10-05 03:48:35 -0700

    (test) Use stainless to better organize tests.

--- tests/single.rs
+++ tests/single.rs
@@ -1,39 +1,80 @@
 #![feature(phase)]
 #[phase(plugin, link)]
 extern crate lazy;
+#[phase(plugin)]
+extern crate stainless;
 
-use lazy::Thunk;
+pub use lazy::Thunk;
+pub use std::sync::{Arc, Mutex};
+pub use std::task;
 
-use std::sync::{Arc, Mutex};
-use std::task;
+describe! thunk {
+    it "should evaluate when accessed" {
+        let val = lazy!(7i);
+        assert_eq!(*val, 7i);
+    }
 
-#[test] fn test_evaluates() {
-    let val = lazy!(7i);
-    assert_eq!(*val, 7i);
+    it "should evaluate just once" {
+        let counter = Arc::new(Mutex::new(0i));
+        let counter_clone = counter.clone();
+        let val = lazy!(*counter.lock() += 1);
+        *val;
+        *val;
+        assert_eq!(*counter_clone.lock(), 1i);
+    }
+
+    it "should not evaluate if not accessed" {
+        let counter = Arc::new(Mutex::new(0i));
+        let counter_clone = counter.clone();
+        let _val = lazy!(*counter.lock() += 1);
+        assert_eq!(*counter_clone.lock(), 0i);
+    }
+
+    describe! methods {
+        it "should allow mutable trait methods" {
+            let mut val = lazy!(7u);
+            val.mutable_borrow_method();
+        }
+    }
+
+    describe! unwrap {
+        it "should retrieve the value" {
+            let val = lazy!(7u);
+            assert_eq!(val.unwrap(), 7u);
+        }
+    }
+
+    describe! evaluated {
+        it "should produce an already evaluated thunk" {
+            let x = Thunk::evaluated(10u);
+            assert_eq!(*x, 10u);
+        }
+    }
+
+    describe! drop {
+        it "should drop internal data just once" {
+            let counter = Arc::new(Mutex::new(0u64));
+            let counter_clone = counter.clone();
+            match task::try(proc() {
+                let value = Dropper(counter_clone);
+                let t = Thunk::<()>::new(proc() {
+                    // Get a reference so value is captured.
+                    let _x = &value;
+
+                    fail!("Muahahahah")
+                });
+                t.force();
+            }) {
+                Err(_) => {
+                    assert_eq!(*counter.lock(), 1);
+                },
+                _ => fail!("Unexpected success in spawned task.")
+            }
+        }
+    }
 }
 
-#[test] fn test_evaluated() {
-    let x = Thunk::evaluated(10u);
-    assert_eq!(*x, 10u);
-}
-
-#[test] fn test_evaluates_just_once() {
-    let counter = Arc::new(Mutex::new(0i));
-    let counter_clone = counter.clone();
-    let val = lazy!(*counter.lock() += 1);
-    *val;
-    *val;
-    assert_eq!(*counter_clone.lock(), 1i);
-}
-
-#[test] fn test_doesnt_evaluate_if_not_accessed() {
-    let counter = Arc::new(Mutex::new(0i));
-    let counter_clone = counter.clone();
-    let _val = lazy!(*counter.lock() += 1);
-    assert_eq!(*counter_clone.lock(), 0i);
-}
-
-//trait ImmutableReferenceTrait {
+//pub trait ImmutableReferenceTrait {
 //    fn borrow_method(&self) { () }
 //}
 //
@@ -45,21 +86,11 @@ use std::task;
 //    val.borrow_method();
 //}
 
-trait MutableReferenceTrait {
+pub trait MutableReferenceTrait {
     fn mutable_borrow_method(&mut self) { () }
 }
 
 impl MutableReferenceTrait for uint {}
-
-#[test] fn test_call_mutable_trait_methods() {
-    let mut val = lazy!(7u);
-    val.mutable_borrow_method();
-}
-
-#[test] fn test_unwrap() {
-    let val = lazy!(7u);
-    assert_eq!(val.unwrap(), 7u);
-}
 
 pub struct Dropper(Arc<Mutex<u64>>);
 
@@ -68,26 +99,6 @@ impl Drop for Dropper {
         let Dropper(ref count) = *self;
         *count.lock() += 1;
         assert!(task::failing())
-    }
-}
-
-#[test] fn test_calls_destructor_once() {
-    let counter = Arc::new(Mutex::new(0u64));
-    let counter_clone = counter.clone();
-    match task::try(proc() {
-        let value = Dropper(counter_clone);
-        let t = Thunk::<()>::new(proc() {
-            // Get a reference so value is captured.
-            let _x = &value;
-
-            fail!("Muahahahah")
-        });
-        t.force();
-    }) {
-        Err(_) => {
-            assert_eq!(*counter.lock(), 1);
-        },
-        _ => faik!("Unexpected success in spawned task.")
     }
 }
 
--- tests/sync.rs
+++ tests/sync.rs
@@ -1,39 +1,44 @@
 #![feature(phase)]
 #[phase(plugin, link)]
 extern crate lazy;
+#[phase(plugin)]
+extern crate stainless;
 
-use lazy::SyncThunk;
+pub use lazy::SyncThunk;
+pub use std::sync::{Arc, Mutex};
 
-use std::sync::{Arc, Mutex};
+describe! sync {
+    it "should evaluate when accessed" {
+        let val = sync_lazy!(7i);
+        assert_eq!(*val, 7i);
+    }
 
-#[test] fn test_evaluates() {
-    let val = sync_lazy!(7i);
-    assert_eq!(*val, 7i);
-}
+    it "should evaluate just once" {
+        let counter = Arc::new(Mutex::new(0i));
+        let counter_clone = counter.clone();
+        let val = sync_lazy!(*counter.lock() += 1);
+        *val;
+        *val;
+        assert_eq!(*counter_clone.lock(), 1i);
+    }
 
-#[test] fn test_evaluates_just_once() {
-    let counter = Arc::new(Mutex::new(0i));
-    let counter_clone = counter.clone();
-    let val = sync_lazy!(*counter.lock() += 1);
-    *val;
-    *val;
-    assert_eq!(*counter_clone.lock(), 1i);
-}
+    it "should not evaluate if not accessed" {
+        let counter = Arc::new(Mutex::new(0i));
+        let counter_clone = counter.clone();
+        let _val = sync_lazy!(*counter.lock() += 1);
+        assert_eq!(*counter_clone.lock(), 0i);
+    }
 
-#[test] fn test_doesnt_evaluate_if_not_accessed() {
-    let counter = Arc::new(Mutex::new(0i));
-    let counter_clone = counter.clone();
-    let _val = sync_lazy!(*counter.lock() += 1);
-    assert_eq!(*counter_clone.lock(), 0i);
-}
+    it "should be send and sync" {
+        Arc::new(sync_lazy!(0u));
+    }
 
-#[test] fn test_is_sync_and_send() {
-    let _ = Arc::new(sync_lazy!(0u));
-}
-
-#[test] fn test_evaluated() {
-    let x = SyncThunk::evaluated(10u);
-    assert_eq!(*x, 10u);
+    describe! evaluated {
+        it "should create an already evaluated thunk" {
+            let x = SyncThunk::evaluated(10u);
+            assert_eq!(*x, 10u);
+        }
+    }
 }
 
 // sync is all safe code, so no need to test
