b65ede0a85f901bc36d82a27ec5d5fcfa30bbbf3
Jonathan Reem <jonathan.reem@gmail.com>
2014-12-02 01:27:16 -0500

    (feat) Migrate SyncThunk to OnceMutex.

--- src/sync.rs
+++ src/sync.rs
@@ -1,26 +1,26 @@
-use std::sync::RWLock;
+use oncemutex::OnceMutex;
 use std::mem;
 
-use self::SyncInner::{Evaluated, EvaluationInProgress, Unevaluated};
+use self::Inner::{Evaluated, EvaluationInProgress, Unevaluated};
 
 /// A sometimes cleaner name.
-pub type SyncLazy<T> = SyncThunk<T>;
+pub type Lazy<T> = Thunk<T>;
 
 /// Sync, Send lazy data.
-pub struct SyncThunk<T> {
-    inner: RWLock<SyncInner<T>>
+pub struct Thunk<T> {
+    inner: OnceMutex<Inner<T>>
 }
 
-impl<T: Send + Sync> SyncThunk<T> {
+impl<T: Send + Sync> Thunk<T> {
     /// Create a new sync thunk.
     ///
-    /// You can construct SyncThunk's manually using this, but the
+    /// You can construct Thunk's manually using this, but the
     /// sync_lazy! macro is preferred.
     ///
     /// ```rust
-    /// # use lazy::SyncThunk;
+    /// # use lazy::sync::Thunk;
     /// # use std::sync::Arc;
-    /// let expensive = SyncThunk::new(proc() { println!("Evaluated!"); 7u });
+    /// let expensive = Thunk::new(proc() { println!("Evaluated!"); 7u });
     /// let reff = Arc::new(expensive);
     /// let reff_clone = reff.clone();
     ///
@@ -30,82 +30,70 @@ impl<T: Send + Sync> SyncThunk<T> {
     /// });
     /// assert_eq!(**reff, 7u);
     /// ```
-    pub fn new(producer: proc(): Send + Sync -> T) -> SyncThunk<T> {
-        SyncThunk { inner: RWLock::new(Unevaluated(producer)) }
+    pub fn new(producer: proc(): Send + Sync -> T) -> Thunk<T> {
+        Thunk { inner: OnceMutex::new(Unevaluated(producer)) }
     }
 
     /// Create a new, evaluated, thunk from a value.
-    pub fn evaluated(val: T) -> SyncThunk<T> {
-        SyncThunk { inner: RWLock::new(Evaluated(val)) }
+    pub fn evaluated(val: T) -> Thunk<T> {
+        let mutex = OnceMutex::new(Evaluated(val));
+
+        // Since we use the invariants of the OnceMutex later,
+        // we have to ensure that they are upheld in this case
+        // by using up our lock.
+        mutex.lock();
+
+        Thunk { inner: mutex }
     }
 
     /// Force evaluation of a thunk.
     pub fn force(&self) {
-        // Take out only a read lock.
-        match *self.inner.read() {
-            // Already forced. We're done and only took a read lock.
-            Evaluated(_) => return,
-
-            // Can't happen because this requires someone else to have
-            // a write lock at the same time.
-            EvaluationInProgress => unreachable!(),
-
-            // First ones here. Evaluate.
-            Unevaluated(_) => ()
+        match self.inner.lock() {
+            // We are the thread responsible for doing the evaluation.
+            Some(mut lock) => {
+                match mem::replace(&mut *lock, EvaluationInProgress) {
+                    Unevaluated(producer) => *lock = Evaluated(producer()),
+                    // Since the OnceMutex only lets us get here once,
+                    // it *must* contain Unevaluated.
+                    _ => unsafe { debug_unreachable!() }
+                }
+            },
+            // Already forced, do nothing.
+            None => {}
         }
-
-        // Get a write lock for the entire evaluation period.
-        let mut write_lock = self.inner.write();
-
-        match *write_lock {
-            // If two threads try to call force at the same time,
-            // then the write locks may be queued up and inner may
-            // have been evaluated already.
-            Evaluated(_) => return,
-            _ => ()
-        }
-
-        // Set the status to EvaluationInProgress
-        match mem::replace(&mut *write_lock, EvaluationInProgress) {
-            // Get the producer, evaluate it.
-            Unevaluated(producer) => *write_lock = Evaluated(producer()),
-
-            // We checked these possibilities earlier.
-            _ => unreachable!()
-        };
     }
 }
 
-impl<T: Send + Sync> DerefMut<T> for SyncThunk<T> {
+impl<T: Send + Sync> DerefMut<T> for Thunk<T> {
     fn deref_mut(&mut self) -> &mut T {
         self.force();
-        match &mut *self.inner.write() {
+        match *&mut*self.inner {
             // Safe because getting this &'a mut T requires &'a mut self.
             //
             // We can't use copy_mut_lifetime here because self is already
-            // borrowed as &mut by self.inner.write().
-            &Evaluated(ref mut val) => unsafe { mem::transmute(val) },
+            // borrowed as &mut by val.
+            Evaluated(ref mut val) => unsafe { mem::transmute(val) },
 
             // We just forced this thunk.
-            _ => unreachable!()
+            _ => unsafe { debug_unreachable!() }
         }
     }
 }
 
-impl<T: Send + Sync> Deref<T> for SyncThunk<T> {
+impl<T: Send + Sync> Deref<T> for Thunk<T> {
     fn deref(&self) -> &T {
         self.force();
-        match *self.inner.read() {
+        match *self.inner {
             // Safe because getting this &'a T requires &'a self.
             Evaluated(ref val) => unsafe { mem::copy_lifetime(self, val) },
 
             // We just forced this thunk.
-            _ => unreachable!()
+            _ => unsafe { debug_unreachable!() }
         }
     }
 }
 
-enum SyncInner<T> {
+enum Inner<T> {
     Evaluated(T),
     EvaluationInProgress,
     Unevaluated(proc(): Send + Sync -> T)
